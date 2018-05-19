1f0f90f9519db0fc1551667b7a687e7862d63727
Zac Willmington <zac.willmington@gmail.com>
2018-05-19 00:46:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
     # binding.pry
     if !params[:username].empty? && !params[:password].empty? && user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect to '/account'
     else
         redirect to '/failure'
