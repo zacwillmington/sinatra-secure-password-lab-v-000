7ef78bc3a387285228a66db583d40d9b965cfb3c
Zac Willmington <zac.willmington@gmail.com>
2018-05-19 00:21:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     user = User.new(:username => params[:username], :password => params[:password])
     binding.pry
     if user.username.empty? || user.password.empty?
-        erb :failure
+        redirect to '/failure'
         binding.pry
     else
         # user.save && !user.username.empty?
