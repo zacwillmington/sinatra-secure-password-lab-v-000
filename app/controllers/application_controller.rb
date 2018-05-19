c876db4bf8095ad1d912d8d922454324661b6d7e
Zac Willmington <zac.willmington@gmail.com>
2018-05-19 00:21:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+      binding.pry
     user = User.new(:username => params[:username], :password => params[:password])
     binding.pry
     if user.username.empty? || user.password.empty?
